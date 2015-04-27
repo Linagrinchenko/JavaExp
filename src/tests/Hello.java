5e9a97102e41040c19c90024d7e76f197c9736a3
lhrynchenko <grinchenkolina@gmail.com>
2015-04-27 17:56:07 +0300

    Revert: Update Hello.java (00ac8df6ae721c5f029f3e83de6b25638181c9bb)

--- src/tests/Hello.java
+++ src/tests/Hello.java
@@ -1,12 +1,10 @@
 /**
  * Created by lhrynchenko on 15.04.2015.
- * 
- * test comment
  */
 public class Hello {
     public static void main(String[] args) {
 
     System.out.println("Hello world!");
-}
+    }
 
 }
